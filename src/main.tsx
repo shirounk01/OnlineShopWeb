5e2f512b7aa3d7a57cd09b889eef9a2f9da2f8a9
shirounk01 <raresmihai18@gmail.com>
2025-11-13 17:17:27 +0200

    first commit

---
+++ src/main.tsx
@@ -0,0 +1,15 @@
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import "./index.css";
+import App from "./App.tsx";
+
+const container = document.getElementById("root");
+if (container) {
+  const root = createRoot(container);
+
+  root.render(
+    <BrowserRouter>
+      <App></App>
+    </BrowserRouter>
+  );
+}
